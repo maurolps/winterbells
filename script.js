81ea0aea38c04c815c3255995b6190a799659216
maurolps <boroengenharia@gmail.com>
2023-09-05 17:19:27 -0300

    Added firebase cloud

--- script.js
+++ script.js
@@ -1,33 +1,58 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js';
+
+const firebaseConfig = {
+ //...
+}; 
+const app = initializeApp(firebaseConfig); 
+const db = getFirestore();
+
 const UpdateDisplay = (() => {
 
-  const updateDayLeft = () => {
-    const daysLeft = document.getElementById('days-left');
-    const myDate = new Date();
-    const day = myDate.getDate();
-    const month = myDate.getMonth() + 1;
-    const year = myDate.getFullYear();
-    let daysCounter = day;
-    for (i = month; i < 13; i++) {
-      let daysInMonth = new Date(year, i, 0).getDate();
-      if (i == month){
-        daysCounter = daysInMonth - daysCounter;
-      } else {
-        if (i==12) { 
-          daysInMonth = 25 
-        }
-        daysCounter += daysInMonth;
-        if (i >= 15) {
-          console.log ("ERROR. Months counter overloaded.");  
-          return null;
-        }
-      }
-    }
-    console.log(daysCounter);
-    daysLeft.textContent = `${daysCounter} DAYS`;
-  }
+ const updateDayLeft = () => {
+   const daysLeft = document.getElementById('days-left');
+   const myDate = new Date();
+   const day = myDate.getDate();
+   const month = myDate.getMonth() + 1;
+   const year = myDate.getFullYear();
+   let daysCounter = day;
+   for (let i = month; i < 13; i++) {
+     let daysInMonth = new Date(year, i, 0).getDate();
+     if (i == month){
+       daysCounter = daysInMonth - daysCounter;
+     } else {
+       if (i==12) { 
+         daysInMonth = 25 
+       }
+       daysCounter += daysInMonth;
+       if (i >= 15) {
+         console.log ("ERROR. Months counter overloaded.");  
+         return null;
+       }
+     }
+   }
+   daysLeft.textContent = `${daysCounter} DAYS`;
+ }
 
-  return {updateDayLeft}
+ return {updateDayLeft}
 
 })()
+//day = user.data().Date.toDate().getDate() -- 5
+//month = user.data().Date.toDate().getMonth() -- 8 (+1 = 9)
+//year = user.data().Date.toDate().getFullYear() -- 2023
+//GIBRALTAR HAVE SEEN THE LIGHTS IN - '03 • '06 • '11
+async function readDB() {
+  try {
+    const userList = collection(db, 'userlist');
+    const data = await getDocs(userList);
+    data.forEach((user) => {
+      console.log(user.data().Name, '=>', user.data().Date.toDate().getFullYear());    
+      
+    });
+  } catch (error) {
+    console.error('Error: ', error); 
+  }
+}
 
 UpdateDisplay.updateDayLeft();
+readDB();
