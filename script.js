4efdaabe46bd6ffc6573a5969bdf50f59a63074c
maurolps <boroengenharia@gmail.com>
2023-09-05 20:14:31 -0300

    Github pages update

--- script.js
+++ script.js
@@ -1,33 +1,115 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+import { getFirestore, collection, getDocs, addDoc } from 'https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js';
+
+const firebaseConfig = {
+//...
+}; 
+
+const app = initializeApp(firebaseConfig); 
+const db = getFirestore(app);
+
 const UpdateDisplay = (() => {
 
-  const updateDayLeft = () => {
-    const daysLeft = document.getElementById('days-left');
-    const myDate = new Date();
-    const day = myDate.getDate();
-    const month = myDate.getMonth() + 1;
-    const year = myDate.getFullYear();
-    let daysCounter = day;
-    for (i = month; i < 13; i++) {
-      let daysInMonth = new Date(year, i, 0).getDate();
-      if (i == month){
-        daysCounter = daysInMonth - daysCounter;
-      } else {
-        if (i==12) { 
-          daysInMonth = 25 
-        }
-        daysCounter += daysInMonth;
-        if (i >= 15) {
-          console.log ("ERROR. Months counter overloaded.");  
-          return null;
-        }
-      }
-    }
-    console.log(daysCounter);
-    daysLeft.textContent = `${daysCounter} DAYS`;
-  }
+ const updateDayLeft = () => {
+   const daysLeft = document.getElementById('days-left');
+   const myDate = new Date();
+   const day = myDate.getDate();
+   const month = myDate.getMonth() + 1;
+   const year = myDate.getFullYear();
+   let daysCounter = day;
+   for (let i = month; i < 13; i++) {
+     let daysInMonth = new Date(year, i, 0).getDate();
+     if (i == month){
+       daysCounter = daysInMonth - daysCounter;
+     } else {
+       if (i==12) { 
+         daysInMonth = 25 
+       }
+       daysCounter += daysInMonth;
+       if (i >= 15) {
+         console.log ("ERROR. Months counter overloaded.");  
+         return null;
+       }
+     }
+   }
+   daysLeft.textContent = `${daysCounter} DAYS`;
+ }
 
-  return {updateDayLeft}
+ const updateUserList = (userData) => {
+  const userList = document.querySelector('.user-list-container');
+  const defaultList = document.querySelectorAll('.user-list');
+  const userListDB = document.querySelector('.user-list-db');
+  const newUserData = document.createElement('p');
+
+  newUserData.className = "text-center md:text-left";
+  newUserData.textContent = userData.toUpperCase();
+  
+  defaultList.forEach((defaultlist) => {
+    defaultlist.remove();
+  });
+
+  if (!userListDB) {
+    const newUserList = document.createElement('div');
+    newUserList.className = "user-list-db";
+    newUserList.appendChild(newUserData);
+    userList.appendChild(newUserList);
+  } else {
+    userListDB.appendChild(newUserData);
+  }
+  
+  
+
+ } 
+
+ return {updateDayLeft, updateUserList}
 
 })()
 
+async function readDB() {
+  try {
+    const userList = collection(db, 'userlist'); 
+    const data = await getDocs(userList);
+    data.forEach((user) => {
+      let userData = "";
+      let day = user.data().Date.toDate().getDate();
+      let month = user.data().Date.toDate().getMonth() + 1;
+      const year = user.data().Date.toDate().getFullYear();
+
+      day = String(day).padStart(2, '0');
+      month = String(month).padStart(2, '0');
+      userData = `${user.data().Name} HAVE SEEN THE LIGHTS IN - '${day} • '${month} • '${year}`;
+      UpdateDisplay.updateUserList(userData);
+    });
+  } catch (error) {
+    console.error('Error: ', error);
+  }
+}
+
+function writeDB () {
+
+   const userName = document.getElementById('checkin').value; 
+   const userList = collection(db, 'userlist');
+   const myDate = new Date();
+
+   addDoc(userList, {
+    Name: userName,
+    Date: myDate,
+   })
+    .catch((error) => {
+      console.error('Error: ', error);
+    })
+ 
+
+}
+
+const formListener = (() => {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    writeDB();
+    form.reset();
+  })
+})();
+
 UpdateDisplay.updateDayLeft();
+readDB();
